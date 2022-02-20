67eeac36434603bc8d8c6f9b63837c0fa1d32119
MaherMohammed <maher.mohammed10.97@gmail.com>
2022-02-20 15:40:01 +0200

    component part 2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import {DISHES} from './shared/dishes.js'
 
 function App() {
 
-  const {dishes,setDishes} = useState(0);
+  const {dishesState,setDishes} = useState({});
   // setDishes(DISHES)
+  // console.log(132)
   return (
     <div className="App">
       <Navbar dark color= "primary">

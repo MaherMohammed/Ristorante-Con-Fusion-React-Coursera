e6536438897386c9dadbaa7c773327d5ca43eab8
MaherMohammed <maher.mohammed10.97@gmail.com>
2022-02-20 15:02:20 +0200

    component part 1 fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-// import logo from './logo.svg';
+import React,{useState} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes.js'
+
 
 function App() {
+
+  const {dishes,setDishes} = useState(0);
+  // setDishes(DISHES)
   return (
     <div className="App">
-      <Navbar dark color="primary">
+      <Navbar dark color= "primary">
         <div className='container'>
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">
+            Con Fusion
+          </NavbarBrand>
         </div>
+
       </Navbar>
-      <Menu />
+
+      <Menu dishes = {DISHES}/>
     </div>
   );
 }

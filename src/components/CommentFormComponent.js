670413f384fcbe47f13ea64bc6a2972583a6be3c
MaherMohammed <maher.mohammed10.97@gmail.com>
2022-03-19 16:29:49 +0200

    task 1 is finishedgit add .

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -32,7 +32,44 @@ class CommentForm extends Component{
                     <ModalHeader toggle = {this.toggleModal}>Submit</ModalHeader>
                     <ModalBody>
                         <LocalForm>
-                            <Label htmlFor = 'Rating'>Rating</Label>
+                            <Row className="form-group">
+                            <Col>
+                                <Label htmlFor = 'rating'>Rating</Label>
+                                <Control.select model='.rating' name = 'rating'
+                                className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Col>
+                            </Row>
+                            <Row className="form-group">
+                            <Col>
+                                <Label htmlFor = 'yourname'>Your Name</Label>
+                                <Control.text model=".yourname" id="yourname" name="yourname"
+                                        placeholder="Your Name"
+                                        className='form-control'
+                                />
+                            </Col>
+                            </Row>
+                            <Row className="form-group">
+                            <Col md={{size: 10}}>
+                                <Label htmlFor = 'comment'>Comment</Label>
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="12"
+                                        className='form-control'
+                                ></Control.textarea>
+                            </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                <Button type="submit" color="primary">
+                                    Submit
+                                </Button>
+                                </Col>
+                            </Row>
                         </LocalForm>
                     </ModalBody>
                 </Modal>
